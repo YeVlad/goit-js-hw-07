a9ce291cefc4146fbc0769e05c19bd0a092230ab
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-03-12 13:34:55 +0100

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const myButton = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const baseColor = document.querySelector('body');
+
+myButton.addEventListener('click', doColor);
+
+function doColor(event) {
+  const newColor = `${getRandomHexColor()}`;
+  spanColor.textContent = newColor;
+  baseColor.style.backgroundColor = spanColor.textContent;
+  // spanColor.style.backgroundColor = newColor;
+}
