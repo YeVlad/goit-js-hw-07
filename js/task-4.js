9c86b4b8b8117ccf9a75c18086d0a29d822a864c
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-03-12 11:00:57 +0100

    4 task done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+const inputEmail = document.querySelector('[name="email"]');
+const inputPassword = document.querySelector('[name="password"]');
+const buttonSubmit = document.querySelector('[type="submit"]');
 
+form.addEventListener('submit', doSubmit);
+
+function doSubmit(event) {
+  event.preventDefault();
+  if (inputEmail.value === '' || inputPassword.value === '') {
+    alert('All form fields must be filled in!');
+  } else {
+    const array = {};
+    const emailName = inputEmail.name;
+    const passwordName = inputPassword.name;
+
+    array[emailName] = inputEmail.value.trim();
+    array[passwordName] = inputPassword.value.trim();
+
+    console.log(array);
+
+    inputEmail.value = '';
+    inputPassword.value = '';
+  }
+}
